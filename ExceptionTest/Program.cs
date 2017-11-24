5e5dbe18b8dca113a838fc7d92140937cdfdb0e0
MSR <mazitov@ukr.net>
2017-11-24 21:23:04 +0200

    Add project files.

---
+++ ExceptionTest/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //long a = long.MaxValue;
+            //Console.WriteLine(a);
+            //try
+            //{
+            //    checked
+            //    {
+            //        long b = a + 1;
+            //        Console.WriteLine(b);
+            //    }
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex.StackTrace);
+            //}
+            try
+            {
+                throw new MyException("RRR");
+            }
+            catch(MyException ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            //try
+            //{
+            //    RunEx();
+            //}
+            //catch(NotImplementedException ex)
+            //{
+            //    Console.WriteLine("---" + ex.StackTrace);
+            //    //throw ex;
+            //}
+
+            //Console.WriteLine("*********************************************");
+
+            //try
+            //{
+            //    var a = 0;
+            //    var o = 1 / a;
+            //}
+            //catch(DivideByZeroException ex)
+            //{
+            //    Console.WriteLine(ex.StackTrace);
+            //    Console.WriteLine("*********************************************");
+            //}
+            //catch (ArithmeticException ex)
+            //{
+            //    Console.WriteLine(ex.StackTrace);
+            //    Console.WriteLine("*********************************************");
+            //}
+
+            Console.ReadKey();   
+        }
+        static void RunEx()
+        {
+            var t = new TestA();
+            t.TestEx();
+        }
+    }
+}
---
+++ ExceptionTest/TestA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionTest
+{
+    class TestA
+    {
+        public void TestEx()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
